536c56785271d20ab4e99910c261a93c29609098
rowbielopezxc <rowbielopez@csu.edu.ph>
2024-06-25 15:24:33 +0800

    updated the backend folder

--- backend/database/entities/index.ts
+++ backend/database/entities/index.ts
@@ -1,4 +1,8 @@
-
+// import { Configuration } from './configuration';
+// import { Event } from './event';
 import { User } from './user';
+// import { Guest } from './guest';
+// import { Feedback } from './feedback';
+// import { Notification } from './notification';
 
-export const ENTITIES = [User, Notification];
+export const ENTITIES = [User, Event, Notification];
